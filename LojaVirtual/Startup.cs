f1edaed9c36867664debff4082081c7d433c28bf
Mariana Antunes <marianajantunes97@gmail.com>
2021-06-02 12:16:00 -0300

    Estrutura inicial

--- LojaVirtual/Startup.cs
+++ LojaVirtual/Startup.cs
@@ -52,14 +52,13 @@ namespace LojaVirtual
             app.UseDefaultFiles();
             app.UseStaticFiles();
             app.UseCookiePolicy();
-            /*
+
             app.UseMvc(routes =>
             {
                 routes.MapRoute(
                     name: "default",
                     template: "{controller=Home}/{action=Index}/{id?}");
             });
-            */
         }
     }
 }
