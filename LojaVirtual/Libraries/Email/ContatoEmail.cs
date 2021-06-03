2e0fe73eda59191aa6da810f0391766adfdaa0c7
Mariana Antunes <marianajantunes97@gmail.com>
2021-06-03 14:55:03 -0300

    Validação com annotations - parte 3

--- LojaVirtual/Libraries/Email/ContatoEmail.cs
+++ LojaVirtual/Libraries/Email/ContatoEmail.cs
@@ -12,20 +12,21 @@ namespace LojaVirtual.Libraries.Email
     {
         public static void EnviarContatoPorEmail(Contato contato)
         {
-            var smtp = new SmtpClient("smtp.gmail.com", 587);
-            smtp.UseDefaultCredentials = false;
-            smtp.Credentials = new NetworkCredential("marianajantunes97@gmail.com", "senha");
-            smtp.EnableSsl = true;
+            var smtp = new SmtpClient("smtp.gmail.com", 587)
+            {
+                UseDefaultCredentials = false,
+                Credentials = new NetworkCredential("marianajantunes97@gmail.com", "senha"),
+                EnableSsl = true
+            };
 
             string corpoMsg = string.Format("<h2>Contato - LojaVirtual</h2>" +
                                             "<b>Nome: </b> {0} <br />" +
                                             "<b>Email: </b> {1} <br />" +
                                             "<b>Texto: </b> {2} <br />" +
-                                            "<br />E-mail enviado automaticamente do site LojaVirtual",
+                                         "<br />E-mail enviado automaticamente do site LojaVirtual",
                                             contato.Nome,
                                             contato.Email,
-                                            contato.Texto
-                                            );
+                                            contato.Texto);
 
             MailMessage mensagem = new MailMessage();
             mensagem.From = new MailAddress("marianajantunes97@gmail.com");
