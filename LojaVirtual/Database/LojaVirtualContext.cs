5f3cda88ea4b434ad70790498758de4f7b10515f
Mariana Antunes <marianajantunes97@gmail.com>
2021-06-07 11:11:41 -0300

    Migrations e layout

--- LojaVirtual/Database/LojaVirtualContext.cs
+++ LojaVirtual/Database/LojaVirtualContext.cs
@@ -14,6 +14,7 @@ namespace LojaVirtual.Database
         }
 
         public DbSet<Cliente> Clientes { get; set; }
+        public DbSet<NewsletterEmail> NewsletterEmails { get; set; }
 
     }
 }
---
+++ LojaVirtual/Migrations/20210607135920_NewsletterEmail.cs
@@ -0,0 +1,39 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+namespace LojaVirtual.Migrations
+{
+    public partial class NewsletterEmail : Migration
+    {
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropPrimaryKey(
+                name: "PK_newsletterEmails",
+                table: "newsletterEmails");
+
+            migrationBuilder.RenameTable(
+                name: "newsletterEmails",
+                newName: "NewsletterEmails");
+
+            migrationBuilder.AddPrimaryKey(
+                name: "PK_NewsletterEmails",
+                table: "NewsletterEmails",
+                column: "Id");
+        }
+
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropPrimaryKey(
+                name: "PK_NewsletterEmails",
+                table: "NewsletterEmails");
+
+            migrationBuilder.RenameTable(
+                name: "NewsletterEmails",
+                newName: "newsletterEmails");
+
+            migrationBuilder.AddPrimaryKey(
+                name: "PK_newsletterEmails",
+                table: "newsletterEmails",
+                column: "Id");
+        }
+    }
+}
