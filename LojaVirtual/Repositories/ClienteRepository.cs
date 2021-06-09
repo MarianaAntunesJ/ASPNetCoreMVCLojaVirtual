0decf3dfe891f2a0c7f0980261d43a930cfa4514
Mariana Antunes <marianajantunes97@gmail.com>
2021-06-09 11:35:07 -0300

    Injeção de dependência

---
+++ LojaVirtual/Repositories/ClienteRepository.cs
@@ -0,0 +1,44 @@
+﻿using LojaVirtual.Database;
+using LojaVirtual.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace LojaVirtual.Repositories
+{
+    public class ClienteRepository : IClienteRepository
+    {
+        private LojaVirtualContext _banco;
+
+        public ClienteRepository(LojaVirtualContext banco)
+        {
+            _banco = banco;
+        }
+
+        public void Atualizar(Cliente cliente)
+        {
+            _banco.Update(cliente);
+            _banco.SaveChanges();
+        }
+
+        public void Cadastrar(Cliente cliente)
+        {
+            _banco.Add(cliente);
+            _banco.SaveChanges();
+        }
+
+        public void Excluir(int id)
+        {
+            _banco.Remove(ObterCliente(id));
+            _banco.SaveChanges();
+        }
+
+        public Cliente Login(string Email, string Senha)
+            => _banco.Clientes.Where(_ => _.Email == Email && _.Senha == Senha).First();
+
+        public Cliente ObterCliente(int id) => _banco.Clientes.Find(id);
+
+        public List<Cliente> ObterTodosClientes() => _banco.Clientes.ToList();
+    }
+}
