f85b6a4b4726249cb7175f0af0f08384ba33ed50
Mariana Antunes <marianajantunes97@gmail.com>
2021-06-14 09:45:12 -0300

    Login Cliente

---
+++ LojaVirtual/Libraries/Login/LoginCliente.cs
@@ -0,0 +1,39 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using LojaVirtual.Libraries.Session;
+using LojaVirtual.Models;
+using Newtonsoft.Json;
+
+namespace LojaVirtual.Libraries.Login
+{
+    public class LoginCliente
+    {
+        private string Key = "Login.Cliente";
+        private Sessao _sessao;
+
+        public LoginCliente(Sessao sessao)
+        {
+            _sessao = sessao;
+        }
+
+        public void Login(Cliente cliente)
+        {
+            var clienteJSONString = JsonConvert.SerializeObject(cliente);
+            _sessao.Cadastrar(Key, clienteJSONString);
+        }
+
+        public Cliente Get(Cliente cliente)
+        {
+            var clienteJSONString = _sessao.Consultar(Key);
+            return JsonConvert.DeserializeObject<Cliente>(clienteJSONString);
+        }
+
+        public void Logout()
+        {
+            _sessao.RemoverTodos();
+        }
+    }
+}
+ 
