120ac829437f26ba01f571e499bd88e7d6745791
Mariana Antunes <marianajantunes97@gmail.com>
2021-06-16 11:42:10 -0300

    Login Colaborador - Area - Colaboradore Classe Login Colaborador

---
+++ LojaVirtual/Libraries/Login/LoginColaborador.cs
@@ -0,0 +1,38 @@
+﻿using LojaVirtual.Libraries.Session;
+using LojaVirtual.Models;
+using Newtonsoft.Json;
+
+namespace LojaVirtual.Libraries.Login
+{
+    public class LoginColaborador
+    {
+        private string Key = "Login.Colaborador";
+        private Sessao _sessao;
+
+        public LoginColaborador(Sessao sessao)
+        {
+            _sessao = sessao;
+        }
+
+        public void Login(Colaborador colaborador)
+        {
+            var colaboradorJSONString = JsonConvert.SerializeObject(colaborador);
+            _sessao.Cadastrar(Key, colaboradorJSONString);
+        }
+
+        public Colaborador Get()
+        {
+            if (_sessao.Existe(Key))
+            {
+                var colaboradorJSONString = _sessao.Consultar(Key);
+                return JsonConvert.DeserializeObject<Colaborador>(colaboradorJSONString);
+            }
+            return null;
+        }
+
+        public void Logout()
+        {
+            _sessao.RemoverTodos();
+        }
+    }
+}
--- LojaVirtual/Startup.cs
+++ LojaVirtual/Startup.cs
@@ -6,16 +6,10 @@ using LojaVirtual.Repositories.Contracts;
 using Microsoft.AspNetCore.Builder;
 using Microsoft.AspNetCore.Hosting;
 using Microsoft.AspNetCore.Http;
-using Microsoft.AspNetCore.HttpsPolicy;
 using Microsoft.AspNetCore.Mvc;
 using Microsoft.EntityFrameworkCore;
-using Microsoft.EntityFrameworkCore.SqlServer;
 using Microsoft.Extensions.Configuration;
 using Microsoft.Extensions.DependencyInjection;
-using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Threading.Tasks;
 
 namespace LojaVirtual
 {
@@ -48,6 +42,7 @@ namespace LojaVirtual
             services.AddSession(options => { });
             services.AddScoped<Sessao>();
             services.AddScoped<LoginCliente>();
+            services.AddScoped<LoginColaborador>();
 
             services.AddMvc().SetCompatibilityVersion(CompatibilityVersion.Version_2_1);
 
@@ -76,6 +71,10 @@ namespace LojaVirtual
 
             app.UseMvc(routes =>
             {
+                routes.MapRoute(
+                    name: "areas",
+                    template: "{area:exists}/{controller=Home}/{action=Index}/{id?}"
+                    );
                 routes.MapRoute(
                     name: "default",
                     template: "{controller=Home}/{action=Index}/{id?}");
